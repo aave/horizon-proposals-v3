d22b866805b56340613df26d3af407b9ecbbb8fa
YBM <31329384+yan-man@users.noreply.github.com>
2026-04-04 00:11:48 -0500

    feat: init flashloanable tx

--- src/AaveV3Horizon_StablecoinFlashloans_20260420/AaveV3Horizon_StablecoinFlashloans_20260420.t.sol
+++ src/AaveV3Horizon_StablecoinFlashloans_20260420/AaveV3Horizon_StablecoinFlashloans_20260420.t.sol
@@ -7,20 +7,20 @@ import {AaveV3EthereumHorizon, AaveV3EthereumHorizonAssets} from 'aave-address-b
 
 /**
  * @dev Test for enabling flashloans on all Horizon stablecoins (GHO, USDC, RLUSD) via multisig transaction.
- * command: FOUNDRY_PROFILE=test forge test --match-contract AaveV3Horizon_StablecoinFlashloans_20260320_Test -vv
+ * command: FOUNDRY_PROFILE=test forge test --match-contract AaveV3Horizon_StablecoinFlashloans_20260420_Test -vv
  */
-contract AaveV3Horizon_StablecoinFlashloans_20260320_Test is ProtocolV3HorizonTestBase {
+contract AaveV3Horizon_StablecoinFlashloans_20260420_Test is ProtocolV3HorizonTestBase {
   // TODO: update OPS_DATA with the actual multisend calldata that enables flashloans on all 3 stablecoins
   address internal constant OPS_TARGET = 0x83Cb1B4af26EEf6463aC20AFbAC9c0e2E017202F;
   bytes internal constant OPS_DATA =
     hex'f213ef0e000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000000000000000000000000000000000000000000001';
-  uint256 internal constant OPS_NONCE = 43;
+  uint256 internal constant OPS_NONCE = 46;
 
   IPoolDataProvider internal constant DATA_PROVIDER =
     IPoolDataProvider(AaveV3EthereumHorizon.AAVE_PROTOCOL_DATA_PROVIDER);
 
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('mainnet'), 24674183);
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 24803845);
   }
 
   function _executeStablecoinFlashloansUpdate() internal {
@@ -32,7 +32,7 @@ contract AaveV3Horizon_StablecoinFlashloans_20260320_Test is ProtocolV3HorizonTe
    */
   function test_defaultProposalExecution() public {
     defaultTest_v3_3(
-      'AaveV3Horizon_StablecoinFlashloans_20260320',
+      'AaveV3Horizon_StablecoinFlashloans_20260420',
       _pool(),
       _executeStablecoinFlashloansUpdate
     );
