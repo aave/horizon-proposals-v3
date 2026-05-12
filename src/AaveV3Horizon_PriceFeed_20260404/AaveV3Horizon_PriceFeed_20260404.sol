19658f9084d98469f727193917f0609df5c804c4
YBM <31329384+yan-man@users.noreply.github.com>
2026-05-12 14:55:34 -0500

    fix: cleanup, comments

--- src/AaveV3Horizon_PriceFeed_20260404/AaveV3Horizon_PriceFeed_20260404.sol
+++ src/AaveV3Horizon_PriceFeed_20260404/AaveV3Horizon_PriceFeed_20260404.sol
@@ -9,11 +9,14 @@ import {IAaveV3ConfigEngine as IEngine} from 'aave-v3-origin/contracts/extension
 /**
  * @title RLUSD & USDC Price Feed Update on Horizon
  * @author Aave Labs
- * @dev Switch RLUSD and USDC oracles to the CAPO adapters (same feeds used on AaveV3Ethereum core).
+ * @dev Update RLUSD and USDC oracles to stable cap adapters.
  */
 contract AaveV3Horizon_PriceFeed_20260404 is AaveV3PayloadHorizonEthereum {
-  address internal constant NEW_RLUSD_ORACLE = 0x9E7c31e9b3C76Ea759D9f7464210353862F0c957; // stable cap adapter
-  address internal constant NEW_USDC_ORACLE = 0x46f94aff8cF7DdC8557eF69f7276087b01C8f363; // stable cap adapter
+  // https://etherscan.io/address/0x9E7c31e9b3C76Ea759D9f7464210353862F0c957
+  address public constant NEW_RLUSD_ORACLE = 0x9E7c31e9b3C76Ea759D9f7464210353862F0c957; // stable cap adapter
+  // https://etherscan.io/address/0x46f94aff8cF7DdC8557eF69f7276087b01C8f363
+  address public constant NEW_USDC_ORACLE = 0x46f94aff8cF7DdC8557eF69f7276087b01C8f363; // stable cap adapter
+
   function priceFeedsUpdates() public pure override returns (IEngine.PriceFeedUpdate[] memory) {
     IEngine.PriceFeedUpdate[] memory updates = new IEngine.PriceFeedUpdate[](2);
     updates[0] = IEngine.PriceFeedUpdate({
